9414114e1f63b9cdf76dabdc2fa45fc4daf7501c
keizhagamink <keizhakamila@upi.edu>
2024-11-21 10:17:42 +0700

    Update page.jsx

--- app/admin/addMap/page.jsx
+++ app/admin/addMap/page.jsx
@@ -57,11 +57,8 @@ const Page = () => {
         <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Write Content Here..' rows={8} required />
         <p className='text-xl mt-4'>Blog Category</p>
         <select name="category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
-          <option value="Startup">Startup</option>
-          <option value="Techology">Technology</option>
-          <option value="Lifesyle">Lifestyle</option>
-          <option value="Technology">Technology</option>
-          <option value="Lifestyle">Lifestyle</option>
+          <option value="Carbon">Carbon</option>
+          <option value="Mangrove">Mangrove</option>
         </select>
         <br />
         <button type='submit' className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
